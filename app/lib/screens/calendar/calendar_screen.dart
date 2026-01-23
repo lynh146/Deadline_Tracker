137d5d58283688630b33dedd5c7590c6f31116a3
dat1606 <trandat0616@gmail.com>
2026-01-24 04:10:01 +0700

    feat: Refactor calendar screens UI and features

--- app/lib/screens/calendar/calendar_screen.dart
+++ app/lib/screens/calendar/calendar_screen.dart
@@ -2,6 +2,9 @@ import 'package:app/core/theme/app_colors.dart';
 import 'package:app/screens/calendar/calendar_month_screen.dart';
 import 'package:app/screens/calendar/calendar_week_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../notifications/notification_bell.dart';
+import '../notifications/notification_screen.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
@@ -11,6 +14,7 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
+  late final String _userId;
   bool _isMonthView = true;
   DateTime _focusedDay = DateTime.now();
 
@@ -25,6 +29,16 @@ class _CalendarScreenState extends State<CalendarScreen> {
     setState(() {
       _isMonthView = true;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw Exception('User chưa đăng nhập');
+    }
+    _userId = user.uid;
   }
 
   @override
@@ -45,11 +59,19 @@ class _CalendarScreenState extends State<CalendarScreen> {
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.notifications_none, color: Colors.black),
-            onPressed: () {
-              // TODO: Handle notification tap
-            },
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: NotificationBell(
+              userId: _userId,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => NotificationScreen(userId: _userId),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
--- app/lib/screens/calendar/calendar_week_screen.dart
+++ app/lib/screens/calendar/calendar_week_screen.dart
@@ -6,7 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class CalendarWeekScreen extends StatefulWidget { //week
+class CalendarWeekScreen extends StatefulWidget {
   final DateTime focusedDay;
   final Function(DateTime) onDaySelected;
 
@@ -45,7 +45,11 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
     final allTasks = await _taskRepository.getTasksByUser(user.uid);
     final Map<DateTime, List<Task>> groupedTasks = {};
     for (final task in allTasks) {
-      final date = DateTime.utc(task.dueAt.year, task.dueAt.month, task.dueAt.day);
+      final date = DateTime.utc(
+        task.dueAt.year,
+        task.dueAt.month,
+        task.dueAt.day,
+      );
       if (groupedTasks[date] == null) {
         groupedTasks[date] = [];
       }
@@ -65,9 +69,9 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
   }
 
   void _navigateToCreateScreen() async {
-    final result = await Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) => const TaskCreateScreen()),
-    );
+    final result = await Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (_) => const TaskCreateScreen()));
 
     if (result == true) {
       _loadAllTasksAndTasksForSelectedDay();
@@ -88,6 +92,7 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
         children: [
           _buildDaysOfWeekHeader(),
           TableCalendar(
+            locale: 'vi_VN',
             firstDay: DateTime.utc(2010, 10, 16),
             lastDay: DateTime.utc(2030, 3, 14),
             focusedDay: _focusedDay,
@@ -99,7 +104,10 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
             headerStyle: const HeaderStyle(
               titleCentered: true,
               formatButtonVisible: false,
-              titleTextStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              titleTextStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             calendarBuilders: CalendarBuilders(
               prioritizedBuilder: (context, day, focusedDay) {
@@ -127,7 +135,10 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
                           padding: const EdgeInsets.only(top: 8.0),
                           child: Text(
                             '${day.day}',
-                            style: TextStyle(color: isSelected ? Colors.white : Colors.black, fontWeight: FontWeight.bold),
+                            style: TextStyle(
+                              color: isSelected ? Colors.white : Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
@@ -136,7 +147,10 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
                           alignment: Alignment.bottomCenter,
                           child: Container(
                             margin: const EdgeInsets.only(bottom: 8),
-                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8,
+                              vertical: 3,
+                            ),
                             decoration: BoxDecoration(
                               color: AppColors.background,
                               borderRadius: BorderRadius.circular(10),
@@ -200,7 +214,17 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: dow.map((day) => Text(day, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12))).toList(),
+        children: dow
+            .map(
+              (day) => Text(
+            day,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+          ),
+        )
+            .toList(),
       ),
     );
   }
@@ -227,7 +251,10 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
         const SizedBox(height: 16),
         const Text(
           'Tạo công việc mới',
-          style: TextStyle(color: AppColors.textPrimary, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: AppColors.textPrimary,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
     );
@@ -242,7 +269,9 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
 
         return Card(
           margin: const EdgeInsets.symmetric(vertical: 8),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
           elevation: 2,
           child: Padding(
             padding: const EdgeInsets.all(16.0),
@@ -252,8 +281,21 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(task.title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                    Text('${task.progress}%', style: const TextStyle(fontWeight: FontWeight.bold, color: AppColors.primary, fontSize: 14)),
+                    Text(
+                      task.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      '${task.progress}%',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.primary,
+                        fontSize: 14,
+                      ),
+                    ),
                   ],
                 ),
                 const SizedBox(height: 8),
@@ -262,7 +304,9 @@ class _CalendarWeekScreenState extends State<CalendarWeekScreen> {
                   child: LinearProgressIndicator(
                     value: progressValue,
                     backgroundColor: AppColors.progressBg,
-                    valueColor: AlwaysStoppedAnimation<Color>(_getTaskProgressColor(task.progress)),
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      _getTaskProgressColor(task.progress),
+                    ),
                     minHeight: 10,
                   ),
                 ),
